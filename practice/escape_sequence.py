f6d6d1c206330b531812acf16306f35735e9d753
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-04-09 21:01:22 +0530

    Understand 'Escape Sequences'

---
+++ practice/escape_sequence.py
@@ -0,0 +1,25 @@
+"""
+Escape Sequence is used when we need to insert or add a certain special character in a string which cannot otherwise added
+some special characters
+1. \n : new/next line
+2. \t : add tab (big space)
+3. \\ : add single backslash
+4. \' : insert a single quote inside a single-quoted string
+5. \" : insert a double quotes inside a double-quoted string
+and many more....
+"""
+
+# \n
+print("Hello Everyone.\nI'm Shreyash Bhagwan Bhoir.\nWelcome to my Github Shreyash-Techspace...")
+
+# \t
+print("Second Year\tAIML")
+
+# \\
+print("Are you interested to explore the world of Technology with me ? yes//no")
+
+# \'
+print('This is my Python\'s Course.')
+
+# \"
+print("how the josh ? \"YES SIR !\"")
