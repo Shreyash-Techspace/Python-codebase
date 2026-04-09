76de4f412cf40fa844d503c0846a32c8c3b3c51f
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-04-09 20:26:27 +0530

    takes user input

--- practice/fstring_in_python.py
+++ practice/fstring_in_python.py
@@ -1,7 +1,12 @@
-name = "John"
+"""name = "John"
 age = 30
 language = "Python"
 hours = 3
+"""
+name = str(input("name: "))
+age = int(input("age: "))
+language = str(input("language: "))
+hours = int(input("hours: "))
 
 # TO PRINT >> John is 20 years old. He studies Python 3 hours a day.
 print(name, "is", age, "year old. He studies", language, hours, "hours a day.")
@@ -9,9 +14,11 @@ print(name, "is", age, "year old. He studies", language, hours, "hours a day.")
 # Using f-string
 print(f"{name} is {age} year old. He studies {language} {hours} hours a day.")
 
-sub1 = 78
-sub2 = 87
-sub3 = 91
-print(f"{name} scored {sub1 + sub2 + sub3} marks in Sem 1.")
+sem = int(input("sem: "))
+sub1 = int(input("sub1: "))
+sub2 = int(input("sub2: "))
+sub3 = int(input("sub3: "))
+
+print(f"{name} scored {sub1 + sub2 + sub3} marks in Sem {sem} .")
 percent = (sub1 + sub2 + sub3) / 3
 print(f"{name} got {percent}% in Sem 1.")
