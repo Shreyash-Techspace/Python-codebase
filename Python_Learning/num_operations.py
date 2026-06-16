6ee9b0fd9b72bc004f98050ce70f0496d3450993
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-06-16 20:34:53 +0530

    Number Operations

---
+++ Python_Learning/num_operations.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# min()
+print(f"Smallest number is {min(numbers)}")
+
+# max()
+print(f"Largest number is {max(numbers)}")
+
+# sum()
+print(f"Total number of numbers is {sum(numbers)}")
