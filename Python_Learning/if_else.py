98d0d086171a0167cbb91962a969e7445989ba42
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-06-27 21:48:23 +0530

    if else statement

---
+++ Python_Learning/if_else.py
@@ -0,0 +1,15 @@
+# if else
+
+"""if condtion:
+    block of code to be executed when condition is True
+else:
+    block of code to be executed when condition is False
+"""
+
+age = float(input("Enter your age: "))
+if age >= 18:
+    print("You are old enough to vote")
+else:
+    print("You are not eligible to vote")
+
+print("Continuous to the Program")
