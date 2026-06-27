65a9c8d013c072f2cd9bb7b83c9f7551d7eca6e4
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-06-27 21:41:11 +0530

    if statement

---
+++ Python_Learning/if.py
@@ -0,0 +1,16 @@
+# ==, >=, <=, >, <, !=
+# Syntax of if :
+"""
+if condition:
+    statement1
+    statement2
+    ...
+    statementN
+statementM
+"""
+
+age = float(input("Enter your age: "))
+if age >= 18:
+    print("You are old enough to vote")
+
+print("Continuous to the Program")
