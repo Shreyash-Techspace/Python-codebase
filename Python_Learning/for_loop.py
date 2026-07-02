33d320b7d1c14f8d4520b9465ff8ce523eaf0451
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-02 19:28:42 +0530

    For Loop

---
+++ Python_Learning/for_loop.py
@@ -0,0 +1,19 @@
+# FOR LOOP
+"""
+- it is an iterator based loop which steps through the items of the collection (list, tuples, sets, dict, str)
+  and executes a block of code repeatedly for a no. of times equal to the items or elements of that collection
+
+- Syntax:
+for var in sequence:
+    statement1
+    statement2
+    ...
+    statementN
+
+Rest of a program
+"""
+
+percentage = [88.7, 90.5, 96.7, 85]
+
+for item in percentage:
+    print(item)
