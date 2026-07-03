4e7f37b4f4f11fd79028425f7724462378617020
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-03 22:40:44 +0530

    Continue and Break

---
+++ Python_Learning/continue_break.py
@@ -0,0 +1,13 @@
+# Continue
+
+for num1 in range(10):
+    if num1 % 3 == 0:
+        continue
+    print(num1)
+
+# Break
+
+for num2 in range(1, 10):
+    if num2 % 3 == 0:
+        break
+    print(num2)
