b3cc75346ac6a49fe25343894c58c54ff79a4ff0
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-26 21:00:39 +0530

    Odd Even Check

--- assignments/Assignment_2_Task_1.py
+++ assignments/Assignment_2_Task_1.py
@@ -5,11 +5,10 @@ Problem Statement:  Write a Python program that:
 2. 	Checks whether the number is even or odd using an if-else statement.
 3. 	Displays the result accordingly.
 """
-
+print("Even Odd Check")
 num = int(input("Enter your number: "))
 
 if num % 2 == 0:
     print(f"The number {num} is an even")
-
 else:
     print(f"The number {num} is an odd")
