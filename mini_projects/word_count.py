eb5f27a51f5ea37e055b0342fc946843e6f45706
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-26 11:56:28 +0530

    Counting and Listing countries with their Initial letter

---
+++ mini_projects/word_count.py
@@ -0,0 +1,13 @@
+countries = ["India", "United States", "Australia", "Ireland", "Sri Lanka", "Iceland", "Cuba", "Iran"]
+
+# Count all the countries which are starting with "I" and list them all
+
+counter = 0
+output = []
+for country in countries:
+    if country[0] == "I":
+        counter += 1
+        output.append(country)
+
+print(f"Found {counter} countries")
+print(output)
