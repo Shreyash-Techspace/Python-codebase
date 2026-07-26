cc0c9195dc5b2d41503930b8202c794177fa0621
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-26 20:44:44 +0530

    Sum of number from 1 to 50

--- assignments/Assignment_2_Task_1.py
+++ assignments/Assignment_2_Task_1.py
@@ -9,7 +9,7 @@ Problem Statement:  Write a Python program that:
 num = int(input("Enter your number: "))
 
 if num % 2 == 0:
-    print(f"The number {num} is even")
+    print(f"The number {num} is an even")
 
 else:
-    print(f"The number {num} is odd")
+    print(f"The number {num} is an odd")
