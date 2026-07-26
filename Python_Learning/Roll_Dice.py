3e572fd92948d7bb4d5cfd7836b26cc6fe4c1882
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-26 11:45:39 +0530

    Rolling a Dice

--- Python_Learning/Roll_Dice.py
+++ Python_Learning/Roll_Dice.py
@@ -4,12 +4,14 @@ print("Welcome to the Game of Rolling a Dice ")
 
 while True:
     choice = input("Press 'Enter' if you want to roll a dice or You can Quit by Pressing a 'q' ")
+    choice = choice.strip()
 
     if choice == 'q':
         print("Thanks for playing the game ")
         break
     elif choice == '':
         c = random.randint(1, 6)
-        print("You rolled a ", c)
+        print(f"Your number is {c}")
     else:
         print("Invalid Choice")
+print("GAME OVER")
