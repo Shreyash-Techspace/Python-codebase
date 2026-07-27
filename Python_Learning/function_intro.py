215114140a696a4ced14cdf4658114be69984a3e
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-27 19:01:38 +0530

    Introduction to Functions

---
+++ Python_Learning/function_intro.py
@@ -0,0 +1,23 @@
+# User-defined functions
+
+# Syntax
+"""
+def function_name(arg1, arg2, arg3, ....., argN):
+    statement1
+    statement2
+    .
+    .
+    .
+    statementN
+"""
+
+
+def greeting_someone(name):
+    print(f"Hello {name}, good morning!")
+    print(f"Its a greeting for you ,{name}")
+
+
+# calling a function
+greeting_someone("Mark")
+greeting_someone("Carol")
+greeting_someone("Nexara")
