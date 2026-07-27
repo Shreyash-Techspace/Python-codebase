7635d92360a58b041b7c088a65b1e6e472998f5d
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-07-27 19:09:10 +0530

    Basic Programs of Functions

---
+++ Python_Learning/user_defined_funcn.py
@@ -0,0 +1,18 @@
+def even_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
+
+
+even_odd(5)
+even_odd(2)
+
+
+def add(num1, num2):
+    result = num1 + num2
+    print(f"The sum is {result}")
+
+
+add(10, 3)
+add(31, 5)
