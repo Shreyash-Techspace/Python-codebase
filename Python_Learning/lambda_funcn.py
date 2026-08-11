cd124c9e93338f2496e3fe2367a90451c947e27c
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-08-11 19:54:13 +0530

    Lambda Function

---
+++ Python_Learning/lambda_funcn.py
@@ -0,0 +1,17 @@
+# Lambda
+
+
+# Traditional Way
+"""
+def add(a,b):
+    return a + b
+res = add(5,2)
+print(res)
+"""
+
+# LAMBDA SYNTAX
+# lambda argument : expression
+
+fun = lambda a, b: a + b
+res = fun(5, 2)
+print(res)
