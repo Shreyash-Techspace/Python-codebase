3ceef2c3c53d43e82e93c1eebe328f51de65da5a
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-08-11 20:53:32 +0530

    User Defined Module

---
+++ Python_Learning/user_define_mod_1.py
@@ -0,0 +1,11 @@
+"""
+A simple Arithmetic module
+"""
+
+
+def add(x, y):
+    return x + y
+
+
+def square_root(x):
+    return x ** 0.5
---
+++ Python_Learning/user_defined_mod_2.py
@@ -0,0 +1,10 @@
+import user_define_mod_1
+
+a = 100
+b = 50
+
+res1 = user_define_mod_1.add(a, b)
+print(res1)
+
+res2 = user_define_mod_1.square_root(a)
+print(res2)
