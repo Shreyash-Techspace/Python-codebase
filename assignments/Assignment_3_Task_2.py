a4b3fd5558d0865bcecd50ed463ffbb968e5080b
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-08-14 18:49:47 +0530

    Assignment 3 Task 2

---
+++ assignments/Assignment_3_Task_2.py
@@ -0,0 +1,14 @@
+# Task 2: Using the Math Module for Calculations
+
+import math
+
+num = int(input("Enter a number: "))
+
+sq_root = math.sqrt(num)
+print(f"Square root of {num} is {sq_root}")
+
+log_e = math.log(num)
+print(f"Logarithm of {num} is {log_e}")
+
+sine = math.sin(num)
+print(f"Sine of {num} is {sine}")
