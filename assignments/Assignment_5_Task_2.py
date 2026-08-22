21cff4c3bbd5d0fbecfa3be39b0fcabc15d190dd
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-08-22 17:25:43 +0530

    Assignment 5 Task 2

---
+++ assignments/Assignment_5_Task_2.py
@@ -0,0 +1,7 @@
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f"Original list: {number_list}")
+extracted = number_list[0:5]
+print(f"Extracted list: {extracted}")
+
+reverse = extracted[::-1]
+print(f"Reversed list: {reverse}")
