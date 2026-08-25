df3435b02dc6e172cb3c842dd71f70448044d59c
Shreyash Bhoir <shreyashbhoir0501@gmail.com>
2026-08-25 22:06:59 +0530

    Object Oriented Programming in Python

---
+++ Python_Learning/OOPS.py
@@ -0,0 +1,29 @@
+"""
+- Object => a container
+         => data -> attributes
+         => functionality => methods/behaviour
+"""
+
+fruits = ["apple", "banana", "cherry"]
+print(type(fruits))
+#  fruits is an object of a class
+fruits.append("orange")
+print(fruits)
+
+"""
+car1
+    => brand = "Mercedes", model = "XYZ124", year = 2024
+    => accelerate, brake
+    
+dot notation (.)
+car1.brand => "Mercedes"
+car1.accelerate(10) 
+"""
+
+# Creating objects => we need classes
+
+# Classes are template/blueprint/design used for creating objects
+# Also called as Type
+
+# Objects are created using the class
+# Instances of a class
